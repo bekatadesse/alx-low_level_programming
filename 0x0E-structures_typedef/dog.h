1bdaed5f5669eeb134f5a0b6a83f46243504424f
bekatadesse <bekatadesse5@gmail.com>
2022-05-10 00:31:08 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
