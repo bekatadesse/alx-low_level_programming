c9853e8fada1af482a9cedace30dbfb7560c391b
bekatadesse <bekatadesse5@gmail.com>
2022-05-10 00:33:25 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,10 @@
 #ifndef STRUCTS
 #define STRUCTS
 /**
- * struct dog - description for a pet
+ * struct dog - 
  * @name: pet name
  * @age: pet age
  * @owner: owner
- *
- * Description: Longer description
  */
 typedef struct dog
 {
@@ -14,3 +12,12 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
