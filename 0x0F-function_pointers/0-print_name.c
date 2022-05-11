a5cadc7f34faaa2f999fa759ee87faa15d638eb8
bekatadesse <bekatadesse5@gmail.com>
2022-05-11 23:18:12 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,19 @@
 #include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
