c7cc9ac79b6df6de8f5a16312b79a17641dcf576
bekatadesse <bekatadesse5@gmail.com>
2022-05-27 15:37:07 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,15 +13,17 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
-	if (index > 63){
+	if (index > 63)
+	{
 		return (-1);
-  }
+	}
 
 	m = 1 << index;
 
-	if (*n & m){
+	if (*n & m)
+	{
 		*n ^= m;
-  }
+	}
 
 	return (1);
 }
