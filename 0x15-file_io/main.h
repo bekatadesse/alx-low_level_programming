8b967527c71d4006203e53f351700965df451c8a
bekatadesse <bekatadesse5@gmail.com>
2022-06-03 15:27:48 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -16,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
